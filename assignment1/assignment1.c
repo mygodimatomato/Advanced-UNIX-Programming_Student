d7e8457fb70910396d3e50f099b41dbf8281285e
Adam West <mygodimatomato@Adams-MacBook-Air.local>
2023-09-16 13:52:22 +0800

    finish assignment 1

---
+++ assignment1/assignment1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+  int fd = open("sample.txt", O_RDWR); // Open file with read-write permissions
+  // int fd = open("sample.txt", O_APPEND); // Open file with append permissions
+  if (fd == -1) {
+    perror("Failed to open file");
+    return 1;
+  }
+
+  char buffer[9];
+  lseek(fd, 14, SEEK_SET);
+  read(fd, buffer, 8);
+  printf("%s\n", buffer);
+
+  // Write "NTHU student."
+  lseek(fd, 14, SEEK_SET);
+  write(fd, "NTHU student.", 13); // Adding NTHU student.
+
+  // Read and print the whole file
+  char finalBuffer[30];
+  lseek(fd, 0, SEEK_SET);
+  read(fd, finalBuffer, 30);
+  printf("%s\n", finalBuffer);
+
+  // close(fd); // Close the file descriptor
+  return 0;
+}
