20c4c0ff62933b202e41755a508c0e3d7b7cb446
Adam West <mygodimatomato@Adams-MacBook-Air.local>
2023-10-13 16:46:52 +0800

    finish the assignment4.c

---
+++ assignment4/assignment4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main() {
+  time_t rawtime;
+  struct tm * timeinfo;
+  char buffer [80];
+
+  //Set the time zone to Asia/Taipei
+  setenv("TZ", "Asia/Taipei", 1);
+  tzset();
+
+  // Obtain the current time
+  time(&rawtime);
+
+  // Convert it to local time
+  timeinfo = localtime(&rawtime);
+
+  // Format the time using strftime
+  strftime(buffer, 80, "%a %b %d %H:%M:%S %Z %Y", timeinfo);
+  printf("Asia/Taipei time:%s\n", buffer);
+
+  // Change the time zone to America/New_York
+  setenv("TZ", "America/New_York", 1);
+  tzset();
+
+  // Obtain the current time again
+  time(&rawtime);
+
+  // Convert it to local time
+  timeinfo = localtime(&rawtime);
+
+  // Format the time using strftime
+  strftime(buffer, 80, "%a %b %d %H:%M:%S %Z %Y", timeinfo);
+  printf("America/New_York time:%s\n", buffer);
+
+  return 0;
+}
