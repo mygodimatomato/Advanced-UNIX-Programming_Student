a67694ab8b7a0a5a937638f177a439ef58ae5499
razetime <rraghu.11502@gmail.com>
2023-12-28 18:43:48 +0800

    q2 final

---
+++ final_coding/q2.c
@@ -0,0 +1,28 @@
+#include <sys/select.h>
+#include <sys/time.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void sleep_us(long int scs) {
+    struct timeval tv;
+    tv.tv_sec=scs/1000000;
+    tv.tv_usec=scs%1000000;
+    select(0,NULL,NULL,NULL,&tv); 
+}
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        puts("Usage: ./q2 [time]");
+        exit(0);
+    }
+    long int scs = atol(argv[1]);
+    struct timeval ct;
+    struct timeval nt;
+    gettimeofday(&ct, NULL);
+    sleep_us(scs);
+    gettimeofday(&nt, NULL);
+    long int fin = (nt.tv_sec-ct.tv_sec)*1000000l+(nt.tv_usec-ct.tv_usec);
+    printf("Sleep time: %lld us\n",fin);
+    return 0;
+}
